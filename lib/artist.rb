a41289b78cb6e5261e786b47363f08c6fef891b6
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-08-01 17:16:09 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,27 @@
 class Artist
-
-  attr_accessor :name, :songs
-
-  @@song_count = 0
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @songs = []
+    @songs = songs
   end
+
 
   def add_song(song)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
     song.artist = self
-    @@song_count += 1
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
 
   def self.song_count
-    @@song_count
+    Song.all.count
   end
-
 end
